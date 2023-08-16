8c3be0182cd201ef9aee8a23306ef2d4f914aa33
Khushboojha13 <106153929+Khushboojha13@users.noreply.github.com>
2023-08-16 23:32:41 +0530

    Add files via upload

---
+++ Fetch Api/index.js
@@ -0,0 +1,30 @@
+//fetch Api has two methods either you use free api through url or you make json and then you use it as url
+let url="https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies";
+let movies= document.getElementsByClassName("movies")[0];
+fetch(url).then((response)=>{
+    return response.json();  //here we have taken the response from url fetch will give promise object usme ham respnse se type of data batana padega here it is json
+}).then((result)=>{ //result me data aaya hai here result is an array
+   console.log(result); 
+
+   //now we want data to in our website so we will array b/c result contains lots of data
+   result.forEach((element)=>{
+       const{Title,Year,Runtime,Poster}=element; //yaha hame jo data json se chahiye vo define karna padega 
+
+       //hame data jo html me card class banayi ha usme leke jana hai so we will make card class here
+       let card=document.createElement("div");
+       card.classList.add("card");
+       //yaha ham card ke innerhtml me card ka content dal rahe 
+       card.innerHTML=`<img src="${Poster}" alt=""> //here we in place of img src we have given json wala data
+
+       <div class="content">
+           <h3>${Title}</h3>
+           <div class="year_time">
+               <h6>${Year}</h6>
+               <h6>${Runtime}</h6>
+               <h6>second</h6>
+           </div>`
+
+           //now we want this above data in movies container
+           movies.appendChild(card);
+   })
+})
